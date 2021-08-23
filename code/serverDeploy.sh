0a490e8358e9b8d98bc098793425a2ff5a35fba1
user <sdohertyp@gmail.com>
2021-08-23 14:18:02 +0000

    updating repo structure

--- code/serverDeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo mysqldump -u root -p bbb_live > ~/bbb_backup.sql
-sudo chown -R bbb:bbb /var/www/html/
-cd /var/www/html/bbbss/
-git pull
-composer install
-composer vendor-exposer
-sudo chown -R apache:apache /var/www/html/
